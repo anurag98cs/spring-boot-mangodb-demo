d6de6ab46fdfe99ef85b1d8a6c16c61688c2510c
Anurag <anurag98cs@gmail.com>
2018-09-11 16:52:45 +0530

    first commit

---
+++ src/main/java/com/example/mangodb/springbootmangodbdemo/SpringBootMangodbDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.mangodb.springbootmangodbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMangodbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMangodbDemoApplication.class, args);
+	}
+}
